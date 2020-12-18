9c4a2ba1b17e6e3b1cdf1f886e26cba3120b2581
ENCYLIN <yanglin.enc@gmail.com>
2020-12-18 08:23:13 +0100

    Revert "update main.py"
    This reverts commit f54d8963

--- main.py
+++ main.py
@@ -13,6 +13,5 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print ('test git sync')
-    print ('i assume on edit-main-in-branch')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
