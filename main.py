f54d89635c934f6984a6f66da8ed8cc5ec6b9e36
ENCYLIN <yanglin.enc@gmail.com>
2020-12-18 08:09:36 +0100

    update main.py

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print ('test git sync')
+    print ('i assume on edit-main-in-branch')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
