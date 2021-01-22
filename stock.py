8a1861f04dc2f10d36b4fb646148d43eb3e268fb
ENCYLIN <yanglin.enc@gmail.com>
2021-01-22 17:48:39 +0100

    python programming for Finance p.2

--- stock.py
+++ stock.py
@@ -1,14 +1,38 @@
 import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
+import yfinance as yf
+
+style.use('ggplot')
+
+start=dt.datetime(2020,1,1)
+end=dt.datetime(2021,1,2)
 
 def get_yahoo_data(start,end):
     df = web.DataReader('TSLA', 'yahoo', start, end)
+    df.to_csv('tsla.csv')
     print(df.tail())
 
-start = dt.datetime(2020, 9, 1)
-end = dt.datetime(2020, 12, 25)
-get_yahoo_data(start,end)
+def read_csv(f):
+    df=pd.read_csv(f,parse_dates=True,index_col=0)
+    print (df.head())
+
+def get_holders():
+#https://towardsdatascience.com/a-comprehensive-guide-to-downloading-stock-prices-in-python-2cd93ff821d4
+    Ticker = yf.Ticker('TSLA')
+    tsla_df = ticker.institutional_holders
+    print (tsla_df.head())
+    tsla_df.to_csv('C:/Users/encylin/PycharmProjects/finance/tsla.csv')
+
+
+#get_yahoo_data(start,end)
+read_csv('tsla.csv')
 
 #read https://einvestingforbeginners.com/yahoo-finance-beginners-guide/
 
+#1 y target Est.
+
+
+#to read:  https://towardsdatascience.com/time-series-forecasting-in-real-life-budget-forecasting-with-arima-d5ec57e634cb
